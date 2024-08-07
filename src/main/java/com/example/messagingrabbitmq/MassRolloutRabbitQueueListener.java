5d4b9489e47ae31d42ca8fe5a3cbbe955b8a16b7
kaiqianyang <kaiqianyang@microsoft.com>
2024-08-07 17:17:02 +0800

    init

--- src/main/java/com/example/messagingrabbitmq/MassRolloutRabbitQueueListener.java
+++ src/main/java/com/example/messagingrabbitmq/MassRolloutRabbitQueueListener.java
@@ -1,26 +1,24 @@
 package com.example.messagingrabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-import com.azure.spring.messaging.servicebus.annotation.ServiceBusListener;
-import com.azure.spring.messaging.implementation.annotation.EnableAzureMessaging;
 
 @Component
 @Slf4j
-@EnableAzureMessaging
 public class MassRolloutRabbitQueueListener {
 
     /**
-     * Listener to consume all the VIN list from MassRollout Service Bus Queue
+     * Kafka Listener to consume all the VIN list from MassRollout Kafka Topic
      *
      * @param message
      */
-    @ServiceBusListener(queues = MessagingRabbitmqApplication.queueName1, containerFactory = "containerFactoryAckAuto")
+    @RabbitListener(queues = MessagingRabbitmqApplication.queueName1, group = "${rabbitmq.firemassrollout.group}", containerFactory = "containerFactoryAckAuto",exclusive = true)
     public void listenMassRolloutEvent(String message) {
         System.out.println("Received from Listener <" + message + ">" + "FROM 1");
     }
 
-    @ServiceBusListener(queues = MessagingRabbitmqApplication.queueName2, containerFactory = "containerFactoryAckAuto")
+    @RabbitListener(queues = MessagingRabbitmqApplication.queueName2, group = "${rabbitmq.firemassrollout.group}", containerFactory = "containerFactoryAckAuto",exclusive = true)
     public void listenMassRolloutEvent2(String message) {
         System.out.println("Received from Listener <" + message + ">" + "FROM 2");
     }
